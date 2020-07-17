df8793fc697732427bc0b2ea723162a38e2b7935
sabatex <serhiy.lakas@hotmail.com>
2020-07-17 12:19:19 +0300

    publish source code

---
+++ demo/Models/DemoModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ntics.Controls.WPFDemo.Models
+{
+    /// <summary>
+    /// Demonstrate entity 
+    /// </summary>
+    public class DemoModel:ObservableObject
+    {
+        public Guid Id { get; set; }
+        string value;
+        public string Value { get=>value; set=>SetProperty(ref this.value,value); }
+        public DemoModel()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+ 
+
+
+
+    }
+}
---
+++ demo/Models/DemoReference.cs
@@ -0,0 +1,25 @@
+﻿using ntics.Controls.WPFDemo.Views;
+using sabatex.WPF.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ntics.Controls.WPFDemo.Models
+{
+    public class DemoReference : IReference<DemoModel>
+    {
+        public IEnumerable<DemoModel> GetItems()
+        {
+            yield return new DemoModel() { Value = "Item 1" };
+            yield return new DemoModel() { Value = "Item 1" };
+            yield return new DemoModel() { Value = "Item 1" };
+            yield return new DemoModel() { Value = "Item 1" };
+            yield return new DemoModel() { Value = "Item 1" };
+        }
+
+        public Type GetSelectedForm()
+        {
+            return typeof(DemoSelectItem);
+        }
+    }
+}
